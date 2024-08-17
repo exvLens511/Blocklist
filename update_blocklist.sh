a2e2413e15955b51c60984f6327b4c8126779b6f
exvLens511 <dennisprnt@gmail.com>
2024-08-17 16:09:11 +0700

    Re-commit update blocklist workflow

--- update_blocklist.sh
+++ update_blocklist.sh
@@ -2,9 +2,9 @@
 
 # Mengatur variabel
 TARGET_DIR=$(pwd)
-SOURCE_URL="https://trustpositif.kominfo.go.id/assets/db/domains_isp"
-GIT_USERNAME="exvLens511"
-GIT_EMAIL="dennisprnt@gmail.com"
+SOURCE_URL="${SOURCE_URL}"
+GIT_USERNAME="${GIT_USERNAME}"
+GIT_EMAIL="${GIT_EMAIL}"
 
 # Hapus file domains_isp lama jika ada
 if [ -f "$TARGET_DIR/domains_isp" ]; then
@@ -21,7 +21,7 @@ git config user.email "$GIT_EMAIL"
 # Tambahkan file ke staging area
 git add domains_isp update_blocklist.sh
 
-# Tambahkan, commit, dan dorong perubahan ke GitHub
+# Commit dan dorong perubahan ke GitHub
 git commit -m "Updated blocklist from Kominfo"
 git pull --rebase origin main
 git push -u origin main
