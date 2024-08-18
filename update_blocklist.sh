65ec8192383df7c9625e2b703a59fab374cb1478
exvLens511 <dennisprnt@gmail.com>
2024-08-18 15:08:08 +0700

    Revert update_blocklist.sh to original state

--- update_blocklist.sh
+++ update_blocklist.sh
@@ -5,7 +5,6 @@ TARGET_DIR=$(pwd)
 SOURCE_URL="${SOURCE_URL}"
 GIT_USERNAME="${GIT_USERNAME}"
 GIT_EMAIL="${GIT_EMAIL}"
-PART_SIZE=100000  # Jumlah baris per file bagian
 
 # Hapus file domains_isp lama jika ada
 if [ -f "$TARGET_DIR/domains_isp" ]; then
@@ -15,13 +14,21 @@ fi
 # Unduh data terbaru dari sumber
 curl --insecure -o "$TARGET_DIR/domains_isp" "$SOURCE_URL"
 
-# Memisahkan file domains_isp menjadi beberapa bagian
-split -l $PART_SIZE "$TARGET_DIR/domains_isp" "$TARGET_DIR/domains_isp_part"
+# Menghitung total baris dalam file domains_isp
+TOTAL_LINES=$(wc -l < "$TARGET_DIR/domains_isp")
+echo "Total lines in domains_isp: $TOTAL_LINES"
+
+# Menghitung jumlah baris per bagian
+LINES_PER_PART=$((($TOTAL_LINES + 2) / 3))
+echo "Lines per part: $LINES_PER_PART"
+
+# Memisahkan file domains_isp menjadi tiga bagian
+split -l $LINES_PER_PART "$TARGET_DIR/domains_isp" "$TARGET_DIR/domains_isp_part"
 
 # Mengganti nama file bagian
-mv "${TARGET_DIR}/domains_isp_parta"* "${TARGET_DIR}/domains_isp_part1"
-mv "${TARGET_DIR}/domains_isp_partb"* "${TARGET_DIR}/domains_isp_part2"
-mv "${TARGET_DIR}/domains_isp_partc"* "${TARGET_DIR}/domains_isp_part3"
+mv "${TARGET_DIR}/domains_isp_partaa" "${TARGET_DIR}/domains_isp_part1"
+mv "${TARGET_DIR}/domains_isp_partab" "${TARGET_DIR}/domains_isp_part2"
+mv "${TARGET_DIR}/domains_isp_partac" "${TARGET_DIR}/domains_isp_part3"
 
 # Git konfigurasi
 git config user.name "$GIT_USERNAME"
