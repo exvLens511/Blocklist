fdcd4020cfdc2feea79fc76372ba7b761df3ad08
exvLens511 <dennisprnt@gmail.com>
2024-08-21 15:18:59 +0700

    Revert update_blocklist.sh to original state

--- update_blocklist.sh
+++ update_blocklist.sh
@@ -5,7 +5,6 @@ TARGET_DIR=$(pwd)
 SOURCE_URL="${SOURCE_URL}"
 GIT_USERNAME="${GIT_USERNAME}"
 GIT_EMAIL="${GIT_EMAIL}"
-KV_NAMESPACE_ID="${KV_NAMESPACE_ID}"
 
 # Hapus file domains_isp lama jika ada
 if [ -f "$TARGET_DIR/domains_isp" ]; then
@@ -42,25 +41,6 @@ git add domains_isp_part1 domains_isp_part2 domains_isp_part3 update_blocklist.s
 git commit -m "Updated blocklist from Kominfo and split into parts"
 git pull --rebase origin main
 git push -u origin main
-
-# Fungsi untuk mengunggah file besar ke KV dalam batch kecil
-upload_to_kv() {
-  local file_path=$1
-  local key_name=$2
-
-  # Memecah file menjadi batch 1000 baris
-  split -l 1000 "$file_path" "${file_path}_part"
-
-  for part in ${file_path}_part*; do
-    local batch_data=$(cat $part)
-    wrangler kv:key put --namespace-id "$KV_NAMESPACE_ID" "$key_name" "$batch_data"
-  done
-}
-
-# Mengunggah setiap bagian ke KV
-upload_to_kv "${TARGET_DIR}/domains_isp_part1" "domains_isp_part1"
-upload_to_kv "${TARGET_DIR}/domains_isp_part2" "domains_isp_part2"
-upload_to_kv "${TARGET_DIR}/domains_isp_part3" "domains_isp_part3"
 
 # Selesai
 echo "Script completed successfully."
